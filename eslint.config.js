079384109d42ec2f82aafd42d01634422523fd67
Marcel Armada-Castellon <marcel.armada@armadacore.com>
2024-12-31 18:25:14 +0100

    chore: Add new scripts and enhance ESLint configuration
    Added `eslint:check` and `type:check` scripts to `package.json` for improved linting and type-checking workflows. Enhanced `eslint.config.js` with support for React version detection, JSX parsing, and updated rules for better coding standards and compatibility.
    
    #10

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,37 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ignores: ['@types/**/*', 'dist/**/*', 'src-tauri/**/*', '__tests/**/*', '*.config.ts', 'tsconfig.json']},
-	{files: ["**/*.{ts,tsx}"]},
-	{languageOptions: {
-		globals: globals.browser,
+	{
+		ignores: ['@types/**/*', 'dist/**/*', 'src-tauri/**/*', '**/__tests__/**/*', '**/*.config.ts', 'tsconfig.json'],
+	},
+	{ files: ['**/*.{ts,tsx}'] },
+	{
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+	{
+		languageOptions: {
+			globals: globals.browser,
 			parserOptions: {
 				projectService: true,
 				tsconfigRootDir: import.meta.dirname,
+				ecmaFeatures: {
+					jsx: true,
+				},
 			},
-	}},
+		},
+	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommendedTypeChecked,
-	pluginReact.configs.flat.recommended,
+	react.configs.flat.recommended,
 	prettierRecommended,
 	{
 		rules: {
@@ -30,7 +44,7 @@ export default [
 			'newline-before-return': 'error',
 			'require-await': 'error',
 
-			"react/react-in-jsx-scope": "off",
+			'react/react-in-jsx-scope': 'off',
 
 			'@typescript-eslint/no-floating-promises': 'error',
 			'@typescript-eslint/no-unused-vars': [
@@ -46,7 +60,7 @@ export default [
 				{
 					selector: 'default',
 					leadingUnderscore: 'allowSingleOrDouble',
-					format: ['camelCase', 'PascalCase', 'UPPER_CASE']
+					format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
 				},
 				{ selector: 'typeLike', format: ['PascalCase'] },
 				{
